3a330832fc4ba21e8ec1874def5252280ae500c0
rubenchu-389 <innovaservices2022@gmail.com>
2024-08-19 22:32:53 -0400

    Hito-2 correccion del navbar y footer

--- web/views.py
+++ web/views.py
@@ -4,12 +4,12 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {})
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html', {})
 
-def welcome(request):
-    return render(request, 'welcome.html')
+def welcome(request): 
+    return render(request, 'welcome.html', {})
 
 
